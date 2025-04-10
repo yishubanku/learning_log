31dea355d025c130d489bafe9bfc7b23c2dc4070
zhuweijie <riiwfnhsou@outlook.com>
2025-04-10 21:06:51 +0800

    feat:添加主题

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -6,6 +6,6 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
-    def __init__(self):
+    def __str__(self):
         """返回模型的字符串表示"""
         return self.text
