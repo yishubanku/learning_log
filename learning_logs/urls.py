20e4e0b2c5f8619fdc0209e7175290fce64db2bf
zhuweijie <riiwfnhsou@outlook.com>
2025-04-13 17:09:18 +0800

    feat:添加显示主题和条目的页面

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,5 +7,9 @@ from . import views
 app_name = 'learning_logs'
 urlpatterns = [
     # 主页
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # 显示所有的主题
+    path('topics/', views.topics, name='topics'),
+    # 显示特定主题的详细页面
+    path('topics/<int:topic_id>/', views.topic, name='topic')
 ]
