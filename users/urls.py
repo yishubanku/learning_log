0de7f274d0f7b4ae85801cf34e47fc844ae44655
zhuweijie <riiwfnhsou@outlook.com>
2025-04-16 16:19:19 +0800

    feat:添加注册页面

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
     # 包含默认的身份验证URL
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    # 注册页面
+    path('register/', views.register, name='register'),
 ]
